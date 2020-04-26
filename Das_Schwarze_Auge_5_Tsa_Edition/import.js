8c05b494a487c1cc8424373f19ef7786cbdf881d
Trestolt <chaven.eleaszar@gmail.com>
2020-04-26 10:59:21 +0200

    add parser

---
+++ Das_Schwarze_Auge_5_Tsa_Edition/import.js
@@ -0,0 +1,185 @@
+//( \n.*? \sAT \s[0-9]{1,3}.*?TP \s.*? \sRW \s.*)   \
+var npc1 = 'Karmanath  \
+Gewicht: kein Gewicht \
+Größe: 1,50 bis 2,00 Schritt (ohne Schwanz); 1,70 bis  \
+2,50 Schritt (mit Schwanz); 1,00 bis 1,20 Schritt  \
+Schulterhöhe  \
+MU 16 KL 10 IN 14 CH 12  \
+FF 10 GE 15 KO 15 KK 15  \
+LeP 25 AsP 25 KaP – INI 19+1W6  \
+VW 10 SK 3 ZK 4 GS 12  \
+Biss: AT 13 TP 1W6+6 RW kurz  \
+Pranke: AT 15 TP 1W6+2 RW kurz  \
+RS/BE: 3/0  \
+Aktionen: 2  \
+Sonderfertigkeiten: Angriff auf ungeschützte Stel-  \
+len (Biss, Pranke), Anspringen (Pranke), Finte I (Biss, \
+Pranke), Kampfreflexe I-III, Verbeißen (Biss) \
+Talente: Einschüchtern 10, Fährtensuchen 16, Klettern \
+3, Körperbeherrschung 12, Kraftakt 9, Schwimmen 4, \
+Selbstbeherrschung 13, Sinnesschärfe 15, Verbergen \
+10, Willenskraft 13 \
+Zauber: Blitz dich find 8, Horriphobus 3 \
+Anzahl: 1 oder 1W3+1 (Dämonenhorde) oder 1W6+4 \
+(Meute) \
+Größenkategorie: mittel \
+Typus: Dämon (niederer, Belshirash), nicht humanoid \
+Anrufungsschwierigkeit: –3 \
+Beute: keine \
+Kampfverhalten: Karmanthi versuchen zunächst, ih- \
+rer Beute Angst zu machen, sie durch Geheul einzu- \
+schüchtern und sie zu hetzen. Am liebsten greifen sie \
+den schwächsten Gegner gemeinsam an und nutzen \
+dabei jeden nur erdenklichen Vorteil wie Finten und \
+Anspringen, um sich anschließend in ihrem Gegner zu \
+verbeißen. \
+Flucht: Karmanthi fliehen nicht. \
+Schmerz +1 bei: immun gegen Schmerz \
+Sphärenkunde (Sphärenwesen): \
+QS 1: Karmanthi kommen vor allem im Norden \
+Aventuriens vor. Sie jagen häufig im Rudel und grei- \
+fen zusammen an. \
+QS 2: In einer Meute sind die Wesen \
+besonders gefährlich, da sie sich \
+gegenseitig antreiben. \
+QS 3+: Diese Dämonen dienen dem \
+eisigen Jäger Nagrach. \
+Sonderregeln: \
+Empfindlichkeit gegenüber gesegneten/geweih- \
+ten Objekten/Waffen: Der Karmanath ist beson- \
+ders empfindlich gegenüber geweihten/gesegneten \
+Objekten/Waffen Firuns. Der gleiche Effekt tritt auch \
+bei anderen mit Winter und Eis verbundenen Gotthei- \
+ten ein, beispielsweise Ifirn. \
+Meute: Karmanthi, die gemeinsam einen Gegner angrei- \
+fen, erhalten pro Karmanath in der Überzahl +1 AT \
+(bis zu einem Maximum von +4 AT). \
+Raserei: Karmanthi werden durch Verletzungen in den \
+Status Raserei versetzt. Sobald sie 5 LeP verloren ha- \
+ben, gelten für sie folgende Modifikatoren: –2 VW, \
++2 AT, +2 TP. Die Modifikatoren sind nicht kumulativ \
+(beim nächsten Verlust von 5 LeP nach den ersten 5 \
+gibt es keinen neuen Bonus, sondern es gilt weiter- \
+hin der alte). Der Status hält an, bis der Gegner getötet \
+wurde, längstens jedoch eine halbe Stunde. \
+Schreckliches Geheul: Karmanthi können ein schreckli- \
+ches Geheul anstimmen und so ihre Gegner das Fürch- \
+ten lehren und sie lähmen. Hören Helden das Geheul, \
+muss eine Vergleichsprobe zwischen Einschüchtern \
+der Karmanthi und Willenskraft (Einschüchtern widerste- \
+hen) der Helden abgelegt werden. Gewinnen die Kar- \
+manthi die Probe, so erleidet der Held pro Netto-QS \
+des Karmanath 1 Stufe Furcht für eine Stunde. Bei \
+zwei oder mehr Netto-QS bekommt der Held zusätz- \
+lich 1 Stufe Paralyse für eine Stunde hinzu. Der Einsatz \
+des Heulens kostet 2 Aktionen. Die Zustandsstufen \
+durch das Heulen sind nicht kumulativ, der Held er- \
+leidet also nur einmal diese Zustände durch das Heu- \
+len, nicht mehrfach. Nach Ablauf der Stunde kann ein \
+Held jedoch erneut davon betroffen sein. \
+Dämonen-Regeln: Für Karmanthi gelten die allgemeinen \
+Dämonen-Regeln (siehe Regelwerk Seite 355).';
+
+var npc2 = 'Hannafrid Altlapper \
+MU 12 KL 10 IN 13 CH 10\
+FF 13 GE 12 KO 13 KK 14\
+LeP 31 AsP – KaP – INI 12+1W6\
+SK 1 ZK 1 AW 6 GS 8\
+Waffenlos: AT 13 PA 8 TP 1W6 RW kurz\
+Knüppel: AT 12 PA 4 TP 1W6+2 RW mittel\
+RS/BE 0/0\
+Sonderfertigkeiten: Wuchtschlag I\
+Vorteile/Nachteile: Verpflichtungen I (gegenüber Bosper)\
+Talente: Körperbeherrschung 5, Kraftakt 6, Selbstbeherrschung\
+5, Sinnesschärfe 2, Verbergen 3, Einschüchtern\
+6, Menschenkenntnis 3, Überreden 3,\
+Willenskraft 4, Pflanzenkunde 10, Tierkunde 8\
+Kampfverhalten: Hannafrid versucht Auseinandersetzungen\
+zu vermeiden und versucht sich gegen\
+Provokationen zunächst mit Einschüchtern zu behaupten.\
+Wenn das nicht gelingt, geht sie aber auch\
+keiner Rauferei aus dem Weg. Sie dient Bosper als\
+Beschützerin und ist bereit, für ihn zu kämpfen.\
+Flucht: flieht nicht \
+Schmerz +1 bei: 23 LeP, 16 LeP, 8 LeP, 5 LeP oder weniger';
+
+var endtoken = 'ENDE99';
+var input = npc2 + endtoken;
+//Keywords to look for
+var keywords = ["Größe:","Gewicht:","MU","KL","IN","CH","FF","GE","KO","KK","LeP","AsP","KaP","INI","VW","SK","ZK","GS","RS/BE","Aktionen","Sonderfertigkeiten","Talente","Zauber","Größenkategorie","Typus","Anrufungsschwierigkeit","Beute","Kampfverhalten","Flucht","Schmerz +1 bei:","Sonderregeln:","Waffenlos","ENDE99"];
+//array for the results
+var multi =[];
+
+keywords.forEach(function( key, index, keywords)
+{   var newArray = [];
+    newArray[0] = key; //add keyword
+    newArray[1] = index; //add keyword index
+    newArray[2] = input.search(key); //add position in text
+    newArray[3] = ''; //result value
+    multi[index] = newArray;
+    //console.log(multi[index][0]+" inloop "+multi[index][1]);
+});
+
+
+
+multi.sort(function(a, b) {
+    var valueA, valueB;
+
+    valueA = a[2]; // Where 1 is your index, from your example
+    valueB = b[2];
+    if (valueA < valueB) {
+        return -1;
+    }
+    else if (valueA > valueB) {
+        return 1;
+    }
+    return 0;
+});
+
+
+//now extract all other elements
+multi.forEach(function( key, index, multi)
+{
+    var start = multi[index][2];
+
+    if (start == -1)
+    { multi[index][3] = '' }
+    else 
+        {
+        if(multi[index][0] == endtoken){
+        //do nothing
+        }
+        else {
+            var next = multi[index+1][2];
+            multi[index][3] = input.substring(start+multi[index][0].length, next).trim();
+           // console.log(key+" inloop "+(start+multi[index][0].length)+" "+next+" "+multi[index][3]);
+       }
+    }
+    
+});
+
+// sort back in keyword order
+
+multi.sort(function(a, b) {
+    var valueA, valueB;
+     // 1 is the index to sort
+    valueA = a[1]; 
+    valueB = b[1];
+    if (valueA < valueB) {
+        return -1;
+    }
+    else if (valueA > valueB) {
+        return 1;
+    }
+    return 0;
+});
+
+
+//do the output
+multi.forEach(function( row, index, multi)
+{console.log(multi[index][0]+' '+multi[index][3]); });
+
+
+
+
+
